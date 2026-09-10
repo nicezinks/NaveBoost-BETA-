a3e82f0ebc2b0535e0e0279b8b1af4181c0ac42e
🄿🅈🅃🄷🄾🄽_🄳🄴🅅 <barbosalany2026@gmail.com>
2026-09-10 17:41:02 -0300

    Delete 20_profile_wizard directory

--- 20_profile_wizard/02_safe_profile_apply.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal enableextensions
-cd /d "%~dp0"
-call "%~dp0..\00_core\common.bat" >nul
-call "%~dp0..\21_adaptive_engine\02_safe_adaptive_apply.bat"
-exit /b %errorlevel%
--- 20_profile_wizard/05_session_report_export.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "%~dp0..\00_core\common.bat" >nul
-set "out=%nb_report%\session_%random%.txt"
-(systeminfo & powercfg /getactivescheme & ipconfig /all) > "%out%"
-echo hardware + energia + rede exportados para %out%
