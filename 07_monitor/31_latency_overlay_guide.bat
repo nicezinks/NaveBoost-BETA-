7edcc499d141e593987e44d2ad6583ad795b2c4d
🄿🅈🅃🄷🄾🄽_🄳🄴🅅 <barbosalany2026@gmail.com>
2026-09-10 17:43:20 -0300

    Delete 07_monitor directory

--- 07_monitor/31_latency_overlay_guide.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "%~dp0..\00_core\common.bat" >nul
-echo use overlay nativo do driver/jogo quando disponivel (ex.: reflex/radeon overlay).
-echo evite empilhar varios overlays durante benchmark.
-pause
