afe7b59702c45910daacb9cbbce5d8d507c33fe8
🄿🅈🅃🄷🄾🄽_🄳🄴🅅 <barbosalany2026@gmail.com>
2026-09-10 17:42:38 -0300

    Delete 16_audio_latency directory

--- 16_audio_latency/01_audio_exclusive_mode_guide.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "%~dp0..\00_core\common.bat" >nul
-mmsys.cpl
-echo configure o dispositivo de saida por propriedades > avancado e teste modo exclusivo por aplicacao.
-pause
--- 16_audio_latency/03_rollback_audio_advanced.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "%~dp0..\00_core\common.bat" >nul
-echo reabra as propriedades do dispositivo e restaure o formato/taxa anteriores registrados no benchmark.
-pause
