549ec0520d473896ba4be2c418975fbb2479dfa6
🄿🅈🅃🄷🄾🄽_🄳🄴🅅 <barbosalany2026@gmail.com>
2026-09-10 17:43:12 -0300

    Delete 15_display_latency directory

--- 15_display_latency/03_refresh_rate_audit.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "%~dp0..\00_core\common.bat" >nul
-powershell.exe -noprofile -command "get-ciminstance -namespace root\wmi -classname wmimonitorid -erroraction silentlycontinue | select instancename,userfriendlyname,serialnumberid | format-list; get-ciminstance win32_videocontroller | select name,currenthorizontalresolution,currentverticalresolution,currentrefreshrate | format-list"
--- 15_display_latency/04_mpo_diagnostic.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call "%~dp0..\00_core\common.bat" >nul
-dxdiag /t "%nb_log%\dxdiag_display.txt" >nul 2>&1
-wevtutil qe system /q:"*[system[(provider[@name='display'])]]" /c:30 /rd:true /f:text > "%nb_log%\display_events.txt" 2>nul
-echo diagnostico salvo em %nb_log%.
