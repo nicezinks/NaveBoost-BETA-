3b0b09d466825dba420ad9093b185a45dd353b92
🄿🅈🅃🄷🄾🄽_🄳🄴🅅 <barbosalany2026@gmail.com>
2026-09-10 17:41:37 -0300

    Delete 00_core directory

--- 00_core/require_admin.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal enableextensions
-fltmc >nul 2>&1
-if not errorlevel 1 exit /b 0
-echo [admin] este recurso precisa de privilegios de administrador.
-echo [admin] execute start_here.bat usando "executar como administrador" e tente novamente.
-exit /b 5
